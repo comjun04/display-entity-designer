fc2eec4436272344c2e926745bb297cb760e0f9f
comjun04 <comjun04@gmail.com>
2024-10-10 13:46:03 +0900

    feat: draw xyz line

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
+import { Canvas } from '@react-three/fiber'
+
 function App() {
   return (
-    <>
-      <h1 className="text-4xl font-bold">hello world</h1>
-    </>
+    <div className="h-full w-full">
+      <Canvas
+        camera={{
+          position: [0.5, 0.5, 0.5],
+          // rotation: [0.0, Math.PI / 4, 0],
+        }}
+      >
+        <line>
+          <bufferGeometry>
+            <bufferAttribute
+              attach="attributes-position"
+              count={2}
+              array={new Float32Array([-100, 0, 0, 100, 0, 0])}
+              itemSize={3}
+            />
+          </bufferGeometry>
+          <lineBasicMaterial color={0xff0000} />
+        </line>
+        <line>
+          <bufferGeometry>
+            <bufferAttribute
+              attach="attributes-position"
+              count={2}
+              array={new Float32Array([0, -100, 0, 0, 100, 0])}
+              itemSize={3}
+            />
+          </bufferGeometry>
+          <lineBasicMaterial color={0x00ff00} />
+        </line>
+        <line>
+          <bufferGeometry>
+            <bufferAttribute
+              attach="attributes-position"
+              count={2}
+              array={new Float32Array([0, 0, -100, 0, 0, 100])}
+              itemSize={3}
+            />
+          </bufferGeometry>
+          <lineBasicMaterial color={0x0000ff} />
+        </line>
+      </Canvas>
+    </div>
   )
 }
 
