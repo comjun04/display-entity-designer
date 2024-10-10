f9686664c193db81832cf69312e1a79fc0c77372
comjun04 <comjun04@gmail.com>
2024-10-10 17:35:46 +0900

    feat: add test box, grid, light, and camera controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { CameraControls, Grid } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+import { useRef } from 'react'
+import { Camera, Color } from 'three'
 
 function App() {
+  const cameraRef = useRef<Camera>(null)
+
   return (
     <div className="h-full w-full">
       <Canvas
+        scene={{
+          background: new Color(0x333333),
+        }}
         camera={{
-          position: [0.5, 0.5, 0.5],
+          position: [3, 3, 3],
           // rotation: [0.0, Math.PI / 4, 0],
+          ref: cameraRef as any,
         }}
       >
         <line>
@@ -42,6 +51,29 @@ function App() {
           </bufferGeometry>
           <lineBasicMaterial color={0x0000ff} />
         </line>
+
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+
+        <ambientLight intensity={0.1} />
+        <directionalLight position={[3, 3, 3]} color="red" />
+
+        <Grid
+          cellSize={1}
+          cellColor={0xffffff}
+          sectionColor={0x0}
+          sectionSize={5}
+          infiniteGrid
+        />
+
+        <CameraControls
+          makeDefault
+          smoothTime={0}
+          draggingSmoothTime={0}
+          maxZoom={1}
+        />
       </Canvas>
     </div>
   )
