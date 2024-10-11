0accf880f7580376b7401ab57b59b4cd8aeb0053
comjun04 <comjun04@gmail.com>
2024-10-12 00:01:18 +0900

    feat: box outline

--- src/components/canvas/Box.tsx
+++ src/components/canvas/Box.tsx
@@ -1,5 +1,10 @@
 import { FC, useMemo } from 'react'
-import { BoxGeometry, MeshStandardMaterial } from 'three'
+import {
+  BoxGeometry,
+  EdgesGeometry,
+  LineBasicMaterial,
+  MeshStandardMaterial,
+} from 'three'
 
 type BoxProps = {
   size: [number, number, number]
@@ -21,12 +26,24 @@ const Box: FC<BoxProps> = ({ color = 0x888888, size, position }) => {
     [color],
   )
 
+  const edgesGeometry = useMemo(() => new EdgesGeometry(geometry), [geometry])
+  const lineMaterial = useMemo(
+    () => new LineBasicMaterial({ color: 'gold' }),
+    [],
+  )
+
   return (
     <object3D position={position}>
       <mesh
         geometry={geometry}
         material={material}
         position={[size[0] / 2, size[1] / 2, size[2] / 2]} // 왼쪽 아래 점이 기준점이 되도록 각 축 변 길이의 반만큼 이동
+      />
+
+      <lineSegments
+        geometry={edgesGeometry}
+        material={lineMaterial}
+        position={[size[0] / 2, size[1] / 2, size[2] / 2]}
       />
     </object3D>
   )
