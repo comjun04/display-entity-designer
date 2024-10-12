f23aca07c9b6ae838c172eeb99100d1466c76e11
comjun04 <comjun04@gmail.com>
2024-10-12 17:23:26 +0900

    feat(Sidebar): show selected entity and ability to select entity

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,28 +1,10 @@
 import { FC } from 'react'
-import { IoCubeOutline } from 'react-icons/io5'
-import { useDisplayEntityStore } from './store'
-import { useShallow } from 'zustand/shallow'
+import ObjectsPanel from './components/sidebar/ObjectsPanel'
 
 const Sidebar: FC = () => {
-  const { entities } = useDisplayEntityStore(
-    useShallow((state) => ({ entities: state.entities })),
-  )
-
   return (
     <div className="w-[400px] p-1">
-      <div className="flex select-none flex-col gap-[2px] bg-neutral-900 p-1 text-sm">
-        <span className="font-bold">Objects</span>
-
-        {entities.map((_, idx) => (
-          <div
-            key={idx}
-            className="flex cursor-pointer flex-row items-center gap-1"
-          >
-            <IoCubeOutline size={16} />
-            <span>asdfasdf</span>
-          </div>
-        ))}
-      </div>
+      <ObjectsPanel />
     </div>
   )
 }
---
+++ src/components/sidebar/ObjectsPanel.tsx
@@ -0,0 +1,37 @@
+import { useDisplayEntityStore } from '@/store'
+import cn from '@/utils'
+import { FC } from 'react'
+import { IoCubeOutline } from 'react-icons/io5'
+import { useShallow } from 'zustand/shallow'
+
+const ObjectsPanel: FC = () => {
+  const { entities, selectedEntity, setSelectedEntity } = useDisplayEntityStore(
+    useShallow((state) => ({
+      entities: state.entities,
+      selectedEntity: state.selectedEntity,
+      setSelectedEntity: state.setSelected,
+    })),
+  )
+
+  return (
+    <div className="flex select-none flex-col gap-[2px] bg-neutral-900 p-1 text-sm">
+      <span className="font-bold">Objects</span>
+
+      {entities.map((entity, idx) => (
+        <div
+          key={idx}
+          className={cn(
+            'flex cursor-pointer flex-row items-center gap-1',
+            selectedEntity?.id === entity.id && 'font-bold text-yellow-500',
+          )}
+          onClick={() => setSelectedEntity(entity.id)}
+        >
+          <IoCubeOutline size={16} />
+          <span>asdfasdf</span>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default ObjectsPanel
