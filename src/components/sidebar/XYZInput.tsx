e6eeca6dcc699564b1de82cffa16f9d49b7a2b2a
comjun04 <comjun04@gmail.com>
2024-10-13 11:04:52 +0900

    fix(XYZInput): accept entering minus character
    input을 별도의 NumberInput으로 분리, 입력 중에는 입력값이 숫자가 아니더라도 강제로 덮어씌우지 않고
    포커스를 잃었을 경우(onBlur)에 다시 덮어씌우도록 변경

--- src/components/sidebar/XYZInput.tsx
+++ src/components/sidebar/XYZInput.tsx
@@ -1,9 +1,44 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+
+type NumberInputProps = {
+  value?: number
+  allowNegative?: boolean
+  onChange?: (num: number) => void
+  className?: string
+}
 
 type XYZInputProps = {
   value: { x: number; y: number; z: number }
   allowNegative?: boolean
   onChange?: (xyz: [number, number, number]) => void
+}
+
+const NumberInput: FC<NumberInputProps> = ({
+  value,
+  allowNegative = false,
+  onChange,
+  className,
+}) => {
+  const [focused, setFocused] = useState(false)
+
+  return (
+    <input
+      type="number"
+      min={allowNegative ? undefined : 0}
+      value={focused ? undefined : value}
+      onChange={(evt) => {
+        if (evt.target.value.length < 1) return
+
+        const num = Number(evt.target.value)
+        if (!isNaN(num) && isFinite(num)) {
+          onChange?.(num)
+        }
+      }}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      className={className}
+    />
+  )
 }
 
 const XYZInput: FC<XYZInputProps> = ({
@@ -15,46 +50,28 @@ const XYZInput: FC<XYZInputProps> = ({
     <div className="flex flex-row justify-stretch gap-2">
       <div className="flex flex-row items-center gap-2">
         <span>X</span>
-        <input
-          type="number"
-          min={allowNegative ? undefined : 0}
+        <NumberInput
           value={value.x}
-          onChange={(evt) => {
-            const num = Number(evt.target.value)
-            if (!isNaN(num) && isFinite(num)) {
-              onChange?.([num, value.y, value.z])
-            }
-          }}
+          allowNegative={allowNegative}
+          onChange={(num) => onChange?.([num, value.y, value.z])}
           className="w-full rounded border-l-4 border-red-500 bg-neutral-800 py-1 pl-1 text-xs outline-none"
         />
       </div>
       <div className="flex flex-row items-center gap-2">
         <span>Y</span>
-        <input
-          type="number"
-          min={allowNegative ? undefined : 0}
+        <NumberInput
           value={value.y}
-          onChange={(evt) => {
-            const num = Number(evt.target.value)
-            if (!isNaN(num) && isFinite(num)) {
-              onChange?.([value.x, num, value.z])
-            }
-          }}
+          allowNegative={allowNegative}
+          onChange={(num) => onChange?.([value.x, num, value.z])}
           className="w-full rounded border-l-4 border-green-500 bg-neutral-800 py-1 pl-1 text-xs outline-none"
         />
       </div>
       <div className="flex flex-row items-center gap-2">
         <span>Z</span>
-        <input
-          type="number"
-          min={allowNegative ? undefined : 0}
+        <NumberInput
           value={value.z}
-          onChange={(evt) => {
-            const num = Number(evt.target.value)
-            if (!isNaN(num) && isFinite(num)) {
-              onChange?.([value.x, value.y, num])
-            }
-          }}
+          allowNegative={allowNegative}
+          onChange={(num) => onChange?.([value.x, value.y, num])}
           className="w-full rounded border-l-4 border-blue-500 bg-neutral-800 py-1 pl-1 text-xs outline-none"
         />
       </div>
