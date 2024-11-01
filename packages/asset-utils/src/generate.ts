f5e15639e68fbc9cab90d718a8444cd6d8f74b99
comjun04 <comjun04@gmail.com>
2024-11-02 03:58:58 +0900

    feat(asset-utils): add hardcoded model data of shulker boxes

--- packages/asset-utils/src/generate.ts
+++ packages/asset-utils/src/generate.ts
@@ -19,9 +19,45 @@ import { BlockstatesFile, ModelFile } from './types'
 
 // =====
 
-const renderableBlockEntityModels = ['chest'].map((type) => `block/${type}`)
+const renderableBlockEntityModels = [
+  'chest',
+  'shulker_box',
+  'black_shulker_box',
+  'blue_shulker_box',
+  'brown_shulker_box',
+  'cyan_shulker_box',
+  'gray_shulker_box',
+  'green_shulker_box',
+  'light_blue_shulker_box',
+  'light_gray_shulker_box',
+  'lime_shulker_box',
+  'magenta_shulker_box',
+  'orange_shulker_box',
+  'pink_shulker_box',
+  'purple_shulker_box',
+  'red_shulker_box',
+  'white_shulker_box',
+  'yellow_shulker_box',
+].map((type) => `block/${type}`)
 const renderableBlockEntityModelTextures = [
   'chest/normal', // chest
+  'shulker/shulker', // shulker_box
+  'shulker/shulker_black',
+  'shulker/shulker_blue',
+  'shulker/shulker_brown',
+  'shulker/shulker_cyan',
+  'shulker/shulker_gray',
+  'shulker/shulker_green',
+  'shulker/shulker_light_blue',
+  'shulker/shulker_light_gray',
+  'shulker/shulker_lime',
+  'shulker/shulker_magenta',
+  'shulker/shulker_orange',
+  'shulker/shulker_pink',
+  'shulker/shulker_purple',
+  'shulker/shulker_red',
+  'shulker/shulker_white',
+  'shulker/shulker_yellow',
 ].map(
   (resourceLocation) =>
     `assets/minecraft/textures/entity/${resourceLocation}.png`,
