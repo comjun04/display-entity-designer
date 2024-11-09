54a62689477c7ada74b776cc5889c5a0b57ca3ec
comjun04 <comjun04@gmail.com>
2024-11-09 11:28:31 +0900

    fix(Model): model xRotation and yRotation not applied properly
    `acacia_button` 등 버튼류 블록들이 x와 y 회전이 동시에 blockstate에 정의되어 있으면 이상하게 회전하는 버그가 있어 수정

--- packages/app/src/components/canvas/Model.tsx
+++ packages/app/src/components/canvas/Model.tsx
@@ -181,12 +181,6 @@ const Model: FC<ModelProps> = ({
     else return stripMinecraftPrefix(textures[key])
   }
 
-  const modelRotation = [
-    MathUtils.degToRad(-xRotation),
-    MathUtils.degToRad(-yRotation),
-    0,
-  ] satisfies [number, number, number]
-
   // display info
 
   const displayInfo = displayType != null ? (display[displayType] ?? {}) : {}
@@ -204,107 +198,120 @@ const Model: FC<ModelProps> = ({
   )
 
   return (
-    <group rotation={modelRotation} position={[0.5, 0.5, 0.5]}>
+    // set y rotation
+    <group
+      rotation={[0, MathUtils.degToRad(-yRotation), 0]}
+      position={[0.5, 0.5, 0.5]}
+    >
       <group position={[-0.5, -0.5, -0.5]}>
-        <group rotation={displayRotation} scale={displayScale}>
-          <group position={displayTranslation}>
-            {elements.map((element, idx) => {
-              const fromVec = new Vector3(...element.from).divideScalar(16)
-              const toVec = new Vector3(...element.to).divideScalar(16)
-              const centerVec = new Vector3()
-                .addVectors(fromVec, toVec)
-                .divideScalar(2)
-              const sizeVec = new Vector3().add(toVec).sub(fromVec)
+        {/* set x rotation */}
+        <group
+          rotation={[MathUtils.degToRad(-xRotation), 0, 0]}
+          position={[0.5, 0.5, 0.5]}
+        >
+          <group position={[-0.5, -0.5, -0.5]}>
+            {/* set display translation, rotation and scale */}
+            <group rotation={displayRotation} scale={displayScale}>
+              <group position={displayTranslation}>
+                {elements.map((element, idx) => {
+                  const fromVec = new Vector3(...element.from).divideScalar(16)
+                  const toVec = new Vector3(...element.to).divideScalar(16)
+                  const centerVec = new Vector3()
+                    .addVectors(fromVec, toVec)
+                    .divideScalar(2)
+                  const sizeVec = new Vector3().add(toVec).sub(fromVec)
 
-              const faces: ReactNode[] = []
-              for (const faceKey in element.faces) {
-                const face = faceKey as ModelFaceKey
-                const faceData = element.faces[face]!
-                const texture = getTexture(faceData.texture)
-                if (texture == null) continue
+                  const faces: ReactNode[] = []
+                  for (const faceKey in element.faces) {
+                    const face = faceKey as ModelFaceKey
+                    const faceData = element.faces[face]!
+                    const texture = getTexture(faceData.texture)
+                    if (texture == null) continue
 
-                const textureLayer =
-                  isItemModel && /^#layer\d{1,}$/.test(faceData.texture)
-                    ? faceData.texture.slice(6)
-                    : undefined
+                    const textureLayer =
+                      isItemModel && /^#layer\d{1,}$/.test(faceData.texture)
+                        ? faceData.texture.slice(6)
+                        : undefined
 
-                faces.push(
-                  <BlockFace
-                    key={face}
-                    modelResourceLocation={initialResourceLocation}
-                    textureResourceLocation={texture}
-                    faceName={face}
-                    uv={faceData.uv}
-                    textureLayer={textureLayer}
-                    rotation={faceData.rotation}
-                    tintindex={faceData.tintindex}
-                    parentElementSize={sizeVec.toArray()}
-                    parentElementFrom={element.from}
-                    parentElementTo={element.to}
-                  />,
-                )
-              }
-
-              // element rotation
-              let rotation = [0, 0, 0] as [number, number, number]
-              let groupScale = [1, 1, 1] as [number, number, number]
-              if (element.rotation != null) {
-                const rad = MathUtils.degToRad(element.rotation.angle)
-
-                switch (element.rotation.axis) {
-                  case 'x':
-                    rotation = [rad, 0, 0]
-                    break
-                  case 'y':
-                    rotation = [0, rad, 0]
-                    break
-                  case 'z':
-                    rotation = [0, 0, rad]
-                    break
-                }
-
-                if (element.rotation.rescale) {
-                  const scaleBy = 1 / Math.cos(rad)
-                  switch (element.rotation.axis) {
-                    case 'x':
-                      groupScale = [1, scaleBy, scaleBy]
-                      break
-                    case 'y':
-                      groupScale = [scaleBy, 1, scaleBy]
-                      break
-                    case 'z':
-                      groupScale = [scaleBy, scaleBy, 1]
-                      break
+                    faces.push(
+                      <BlockFace
+                        key={face}
+                        modelResourceLocation={initialResourceLocation}
+                        textureResourceLocation={texture}
+                        faceName={face}
+                        uv={faceData.uv}
+                        textureLayer={textureLayer}
+                        rotation={faceData.rotation}
+                        tintindex={faceData.tintindex}
+                        parentElementSize={sizeVec.toArray()}
+                        parentElementFrom={element.from}
+                        parentElementTo={element.to}
+                      />,
+                    )
                   }
-                }
-              }
 
-              // 회전 중심 위치
-              const rotationOriginVec = new Vector3(
-                ...(element.rotation?.origin ?? ([0, 0, 0] as const)),
-              ).divideScalar(16)
+                  // element rotation
+                  let rotation = [0, 0, 0] as [number, number, number]
+                  let groupScale = [1, 1, 1] as [number, number, number]
+                  if (element.rotation != null) {
+                    const rad = MathUtils.degToRad(element.rotation.angle)
 
-              const vec1 = centerVec.clone().sub(rotationOriginVec)
-              const vec2 = rotationOriginVec
-                .clone()
-                .subScalar(isBlockShapedItemModel ? 0.5 : 0)
+                    switch (element.rotation.axis) {
+                      case 'x':
+                        rotation = [rad, 0, 0]
+                        break
+                      case 'y':
+                        rotation = [0, rad, 0]
+                        break
+                      case 'z':
+                        rotation = [0, 0, rad]
+                        break
+                    }
 
-              // rotation origin 적용할 때
-              // 1. centerVec - rotationOriginVec 위치로 이동 => 회전 중심위치가 (0,0,0)에 위치하도록 함
-              // 2. 부모 group에서 rotation을 적용하고 원래 위치로 다시 움직임 (centerVec - rotationOriginVec + rotationOriginVec = centerVec)
-              return (
-                <group
-                  key={idx}
-                  rotation={rotation}
-                  position={vec2}
-                  scale={groupScale}
-                >
-                  <group position={vec1}>
-                    <Suspense>{faces}</Suspense>
-                  </group>
-                </group>
-              )
-            })}
+                    if (element.rotation.rescale) {
+                      const scaleBy = 1 / Math.cos(rad)
+                      switch (element.rotation.axis) {
+                        case 'x':
+                          groupScale = [1, scaleBy, scaleBy]
+                          break
+                        case 'y':
+                          groupScale = [scaleBy, 1, scaleBy]
+                          break
+                        case 'z':
+                          groupScale = [scaleBy, scaleBy, 1]
+                          break
+                      }
+                    }
+                  }
+
+                  // 회전 중심 위치
+                  const rotationOriginVec = new Vector3(
+                    ...(element.rotation?.origin ?? ([0, 0, 0] as const)),
+                  ).divideScalar(16)
+
+                  const vec1 = centerVec.clone().sub(rotationOriginVec)
+                  const vec2 = rotationOriginVec
+                    .clone()
+                    .subScalar(isBlockShapedItemModel ? 0.5 : 0)
+
+                  // rotation origin 적용할 때
+                  // 1. centerVec - rotationOriginVec 위치로 이동 => 회전 중심위치가 (0,0,0)에 위치하도록 함
+                  // 2. 부모 group에서 rotation을 적용하고 원래 위치로 다시 움직임 (centerVec - rotationOriginVec + rotationOriginVec = centerVec)
+                  return (
+                    <group
+                      key={idx}
+                      rotation={rotation}
+                      position={vec2}
+                      scale={groupScale}
+                    >
+                      <group position={vec1}>
+                        <Suspense>{faces}</Suspense>
+                      </group>
+                    </group>
+                  )
+                })}
+              </group>
+            </group>
           </group>
         </group>
       </group>
