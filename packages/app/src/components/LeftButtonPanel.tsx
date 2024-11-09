caba7f6658eb06a5433cf92d17ab36f5d85ba7c7
comjun04 <comjun04@gmail.com>
2024-11-09 11:50:40 +0900

    feat: add menu button and test menu

--- packages/app/src/components/LeftButtonPanel.tsx
+++ packages/app/src/components/LeftButtonPanel.tsx
@@ -1,9 +1,10 @@
 import { useEditorStore } from '@/store'
 import { FC } from 'react'
 import { IoMove } from 'react-icons/io5'
-import { LuMoveDiagonal, LuRotate3D } from 'react-icons/lu'
+import { LuMenu, LuMoveDiagonal, LuRotate3D } from 'react-icons/lu'
 import { useShallow } from 'zustand/shallow'
 import FloatingButton from './FloatingButton'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 
 const LeftButtonPanel: FC = () => {
   const { mode, setMode } = useEditorStore(
@@ -15,6 +16,20 @@ const LeftButtonPanel: FC = () => {
 
   return (
     <div className="absolute left-0 top-0 z-[5] ml-4 mt-4 flex flex-col gap-2">
+      <Menu>
+        <MenuButton as={FloatingButton}>
+          <LuMenu size={24} />
+        </MenuButton>
+        <MenuItems
+          anchor="right start"
+          className="z-10 ml-2 rounded-lg bg-neutral-800 p-2"
+        >
+          <MenuItem>
+            <a className="block">test menu item</a>
+          </MenuItem>
+        </MenuItems>
+      </Menu>
+
       <FloatingButton
         active={mode === 'translate'}
         onClick={() => setMode('translate')}
