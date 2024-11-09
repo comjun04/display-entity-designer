0c1eb6a155a94b956c295677caf3fca1a4508402
comjun04 <comjun04@gmail.com>
2024-11-10 02:08:33 +0900

    feat(ExportToMinecraftDialog): generate export command

--- packages/app/src/components/dialog/ExportToMinecraftDialog.tsx
+++ packages/app/src/components/dialog/ExportToMinecraftDialog.tsx
@@ -1,4 +1,8 @@
-import { useDialogStore } from '@/store'
+import {
+  useDialogStore,
+  useDisplayEntityStore,
+  useEntityRefStore,
+} from '@/store'
 import {
   Dialog,
   DialogBackdrop,
@@ -15,11 +19,62 @@ const ExportToMinecraftDialog: FC = () => {
       setOpenedDialog: state.setOpenedDialog,
     })),
   )
+  const { entities } = useDisplayEntityStore(
+    useShallow((state) => ({
+      entities: state.entities,
+    })),
+  )
+  const { entityRefs } = useEntityRefStore(
+    useShallow((state) => ({
+      entityRefs: state.entityRefs,
+    })),
+  )
 
-  const closeDialog = () => setOpenedDialog(null)
+  const passengersString = entities
+    .map((entity) => {
+      const refData = entityRefs.find((e) => e.id === entity.id)
+      if (refData == null || refData.objectRef.current == null) {
+        console.warn(`entity ref of entity ${entity.id} not found, ignoring.`)
+        return
+      }
+
+      const worldMatrix = refData.objectRef.current.matrixWorld
+
+      const idText = entity.kind + '_display' // block_display, item_display
+      const transformationString = worldMatrix
+        .clone()
+        .transpose()
+        .toArray()
+        .map((num) => Math.round(num * 1_0000_0000) / 1_0000_0000 + 'f')
+        .join(',')
+
+      let specificData = ''
+      if (entity.kind === 'block') {
+        const propertiesText = Object.entries(entity.blockstates)
+          .map(([k, v]) => `${k}:"${v}"`)
+          .join(',')
+
+        specificData = `block_state:{Name:"${entity.type}",Properties:{${propertiesText}}}`
+      } else if (entity.kind === 'item') {
+        {
+          const displayText =
+            entity.display != null ? `,item_display:"${entity.display}"` : ''
+          specificData = `item:{id:"${entity.type}"}${displayText}`
+        }
+      }
+
+      return `{id:"${idText}",${specificData},transformation:[${transformationString}]}`
+    })
+    .filter((d) => d != null)
+
+  const nbt = `{Passengers:[${passengersString.join(',')}]}`
 
   return (
-    <Dialog open={isOpen} onClose={closeDialog} className="relative z-50">
+    <Dialog
+      open={isOpen}
+      onClose={() => setOpenedDialog(null)}
+      className="relative z-50"
+    >
       <DialogBackdrop
         transition
         className="fixed inset-0 backdrop-blur-sm duration-200 ease-out data-[closed]:opacity-0"
@@ -37,7 +92,7 @@ const ExportToMinecraftDialog: FC = () => {
           <textarea
             className="h-24 resize-none rounded-lg p-2 outline-none"
             readOnly
-            value="/summon block_display ~ ~ ~" // test string
+            value={`/summon block_display ~ ~ ~ ${nbt}`}
           />
         </DialogPanel>
       </div>
