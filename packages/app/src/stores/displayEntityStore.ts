d8d9a5f63321bc4dbddf7bf39b3d40118a23e8df
comjun04 <comjun04@gmail.com>
2024-12-31 13:41:08 +0900

    fix(displayEntityStore): apply `selectionBaseTransformation` value correctly when changing entity selection

--- packages/app/src/stores/displayEntityStore.ts
+++ packages/app/src/stores/displayEntityStore.ts
@@ -12,6 +12,8 @@ import {
   PartialNumber3Tuple,
 } from '@/types'
 
+import { useEditorStore } from './editorStore'
+
 export type DisplayEntityState = {
   entityIds: string[]
   entities: DisplayEntity[]
@@ -84,6 +86,18 @@ export const useDisplayEntityStore = create(
     },
     setSelected: (ids) =>
       set((state) => {
+        if (state.selectedEntityIds[0] !== ids[0]) {
+          const firstSelectedEntity = state.entities.find(
+            (e) => e.id === ids[0],
+          )
+
+          useEditorStore.getState().setSelectionBaseTransformation({
+            position: firstSelectedEntity?.position,
+            rotation: firstSelectedEntity?.rotation,
+            size: firstSelectedEntity?.size,
+          })
+        }
+
         state.selectedEntityIds = ids
       }),
     addToSelected: (id) =>
