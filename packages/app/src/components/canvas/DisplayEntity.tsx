4251d3212b9faaa15860e6ca5bba14f59e83f3b6
comjun04 <comjun04@gmail.com>
2025-01-18 18:40:45 +0900

    fix(DisplayEntity): remove console warning for missing ref data

--- packages/app/src/components/canvas/DisplayEntity.tsx
+++ packages/app/src/components/canvas/DisplayEntity.tsx
@@ -23,20 +23,23 @@ const DisplayEntity: FC<DisplayEntityProps> = ({ id }) => {
 
   const { thisEntityRef, parentGroupRefData } = useEntityRefStore(
     useShallow((state) => {
-      // parent 값이 있다면
+      // parent 값이 있다면 parent ref data를 찾아서 제공, ref data가 아직 없을 경우 (프로젝트 파일 로드로 인한 동시 entity 생성 등 상황) 임시로 root group ref를 제공
+      // 나중에 parent ref data가 준비되면 store가 업데이트되면서 다시 렌더링되므로 이때 맞는 parent ref data를 찾아서 제공
       const parentGroupRefData =
         thisEntity?.parent != null
           ? state.entityRefs.get(thisEntity.parent)
           : state.rootGroupRefData
 
-      if (
-        thisEntity?.parent != null &&
-        !state.entityRefs.has(thisEntity.parent)
-      ) {
-        console.warn(
-          'thisEntity.parent is not null, but parent entity ref is null',
-        )
-      }
+      // parent 값이 있지만 ref가 없을 경우 무언가 문제가 발생했다고 알려주려 했는데,
+      // 여러 entity를 삭제하는 경우에 이런 현상이 자연스럽게 생길 수 있어서 관련 코드 비활성화
+      // if (
+      //   thisEntity?.parent != null &&
+      //   !state.entityRefs.has(thisEntity.parent)
+      // ) {
+      //   console.warn(
+      //     'thisEntity.parent is not null, but parent entity ref is null',
+      //   )
+      // }
 
       return {
         thisEntityRef: state.entityRefs.get(id),
