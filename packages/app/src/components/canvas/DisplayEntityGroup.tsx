faf5c45654916b7c5dbf38a23e4abc65efcade1a
comjun04 <comjun04@gmail.com>
2025-01-19 17:46:50 +0900

    fix(DisplayEntityGroup): group position does not change on first creation

--- packages/app/src/components/canvas/DisplayEntityGroup.tsx
+++ packages/app/src/components/canvas/DisplayEntityGroup.tsx
@@ -124,6 +124,20 @@ const DisplayEntityGroup: FC<DisplayEntityGroupProps> = ({
     }
   })
 
+  // entity group이 처음 생겼을 경우 transformation 설정 (id와 ref는 바뀔 일이 거의 없음)
+  // 그룹이 생성될 때 자동으로 선택되는데, 이 때문에 아래 useFrame 코드에서 entity의 transformation이 세팅되지 않아 (0, 0, 0)에 있는 문제를 해결결
+  useEffect(() => {
+    const thisEntity = useDisplayEntityStore.getState().entities.get(id)
+    if (thisEntity == null) return
+
+    const { position, rotation, size } = thisEntity
+
+    ref?.current?.position.set(...position)
+    ref?.current?.rotation.set(...rotation)
+    ref?.current?.scale.set(...size)
+  }, [id, ref])
+
+  // 매 프레임 렌더링 시마다 선택되어 있지 않을 경우 transformation 설정
   useFrame(() => {
     if (!thisEntitySelected) {
       ref?.current?.position.set(...position)
