9a2e12277d55eb79d7179f9546b9d34f7522bdb6
comjun04 <comjun04@gmail.com>
2025-01-25 23:01:26 +0900

    feat(SettingsDialog): add hotkeys page

--- packages/app/src/components/dialog/SettingsDialog.tsx
+++ packages/app/src/components/dialog/SettingsDialog.tsx
@@ -11,7 +11,7 @@ import { useDialogStore } from '@/stores/dialogStore'
 import { useEditorStore } from '@/stores/editorStore'
 import { cn } from '@/utils'
 
-type SettingsPageType = 'programInfo' | 'test'
+type SettingsPageType = 'hotkeys' | 'programInfo' | 'test'
 
 const SettingsDialog: FC = () => {
   const { isOpen, setOpenedDialog } = useDialogStore(
@@ -53,6 +53,17 @@ const SettingsDialog: FC = () => {
                 <button
                   className={cn(
                     'w-full rounded px-2 py-1 text-start text-sm transition duration-150',
+                    selectedPage === 'hotkeys'
+                      ? 'bg-neutral-700'
+                      : 'hover:bg-neutral-700/50',
+                  )}
+                  onClick={() => setSelectedPage('hotkeys')}
+                >
+                  Hotkeys
+                </button>
+                <button
+                  className={cn(
+                    'w-full rounded px-2 py-1 text-start text-sm transition duration-150',
                     selectedPage === 'programInfo'
                       ? 'bg-neutral-700'
                       : 'hover:bg-neutral-700/50',
@@ -76,6 +87,78 @@ const SettingsDialog: FC = () => {
             </div>
 
             <div className="h-full w-full rounded-xl bg-neutral-900/70 p-6">
+              {/* Hotkeys */}
+              {selectedPage === 'hotkeys' && (
+                <div>
+                  <h3 className="text-2xl font-bold">Hotkeys</h3>
+                  <div className="mt-2 text-sm text-neutral-500">
+                    *Changing hotkeys are not yet supported.
+                  </div>
+                  <div className="mt-2">
+                    <div className="rounded bg-neutral-800 px-3 py-1 text-gray-400">
+                      General
+                    </div>
+                    <div className="flex flex-col">
+                      <div className="mt-1 flex flex-row items-center gap-2">
+                        <span className="grow px-3">Open from file</span>
+                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                          Ctrl + O
+                        </div>
+                      </div>
+                      <div className="mt-1 flex flex-row items-center gap-2">
+                        <span className="grow px-3">Save to file</span>
+                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                          Ctrl + S
+                        </div>
+                      </div>
+                      <div className="mt-1 flex flex-row items-center gap-2">
+                        <span className="grow px-3">Open Settings</span>
+                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                          Ctrl + ,
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-1">
+                    <div className="rounded bg-neutral-800 px-3 py-1 text-gray-400">
+                      Editor
+                    </div>
+                    <div className="flex flex-col">
+                      <div className="mt-1 flex flex-row items-center gap-2">
+                        <span className="grow px-3">Translate mode</span>
+                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                          T
+                        </div>
+                      </div>
+                      <div className="mt-1 flex flex-row items-center gap-2">
+                        <span className="grow px-3">Rotate mode</span>
+                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                          R
+                        </div>
+                      </div>
+                      <div className="mt-1 flex flex-row items-center gap-2">
+                        <span className="grow px-3">Scale mode</span>
+                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                          S
+                        </div>
+                      </div>
+                      <div className="mt-1 flex flex-row items-center gap-2">
+                        <span className="grow px-3">Group/Ungroup</span>
+                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                          G
+                        </div>
+                      </div>
+                      <div className="mt-1 flex flex-row items-center gap-2">
+                        <span className="grow px-3">Delete Entity</span>
+                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                          Delete
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+
               {/* Program Info */}
               {selectedPage === 'programInfo' && (
                 <div>
