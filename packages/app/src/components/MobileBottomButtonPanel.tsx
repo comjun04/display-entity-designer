41b7a735be5a86e112ad3d7091d4f73710befd3b
comjun04 <comjun04@gmail.com>
2025-06-03 13:41:04 +0900

    feat: add fullscreen button for mobile

--- packages/app/src/components/MobileBottomButtonPanel.tsx
+++ packages/app/src/components/MobileBottomButtonPanel.tsx
@@ -2,6 +2,7 @@ import { LuPanelRightOpen } from 'react-icons/lu'
 import { useShallow } from 'zustand/shallow'
 
 import { useEditorStore } from '@/stores/editorStore'
+import FullscreenToggle from './FullscreenToggle'
 
 const MobileBottomButtonPanel: FC = () => {
   const { mobileSidebarOpened, setMobileSidebarOpened } = useEditorStore(
@@ -13,6 +14,7 @@ const MobileBottomButtonPanel: FC = () => {
 
   return (
     <div className="fixed bottom-4 right-4 flex flex-col gap-2 xs:hidden">
+      <FullscreenToggle />
       <button
         className="rounded-lg bg-black p-2"
         onClick={() => setMobileSidebarOpened(true)}
