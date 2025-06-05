bd11f134f47fe7d71d20f72a59f107556f3985ee
comjun04 <comjun04@gmail.com>
2025-06-05 20:26:21 +0900

    refactor(settings): change dialog design

--- packages/app/src/components/dialog/SettingsDialog.tsx
+++ packages/app/src/components/dialog/SettingsDialog.tsx
@@ -1,9 +1,3 @@
-import {
-  Dialog,
-  DialogBackdrop,
-  DialogPanel,
-  DialogTitle,
-} from '@headlessui/react'
 import { FC, useState } from 'react'
 import { useShallow } from 'zustand/shallow'
 
@@ -11,6 +5,8 @@ import { useDialogStore } from '@/stores/dialogStore'
 import { useEditorStore } from '@/stores/editorStore'
 import { LogLevel } from '@/types'
 import { cn } from '@/utils'
+
+import Dialog from './Dialog'
 
 type SettingsPageType = 'hotkeys' | 'programInfo' | 'test'
 
@@ -34,196 +30,181 @@ const SettingsDialog: FC = () => {
   const closeDialog = () => setOpenedDialog(null)
 
   return (
-    <Dialog open={isOpen} onClose={closeDialog} className="relative z-50">
-      <DialogBackdrop
-        transition
-        className="fixed inset-0 backdrop-blur-sm duration-200 ease-out data-[closed]:opacity-0"
-      />
+    <Dialog
+      title="Settings"
+      open={isOpen}
+      onClose={closeDialog}
+      className="relative z-50"
+    >
+      <div className="flex h-full w-full flex-row">
+        <div className="w-[30%] p-4 border-r-2 border-neutral-700">
+          <div className="mt-2 flex flex-col gap-1">
+            <button
+              className={cn(
+                'w-full rounded px-2 py-1 text-start text-sm transition duration-150',
+                selectedPage === 'hotkeys'
+                  ? 'bg-neutral-700'
+                  : 'hover:bg-neutral-700/50',
+              )}
+              onClick={() => setSelectedPage('hotkeys')}
+            >
+              Hotkeys
+            </button>
+            <button
+              className={cn(
+                'w-full rounded px-2 py-1 text-start text-sm transition duration-150',
+                selectedPage === 'programInfo'
+                  ? 'bg-neutral-700'
+                  : 'hover:bg-neutral-700/50',
+              )}
+              onClick={() => setSelectedPage('programInfo')}
+            >
+              Program Info
+            </button>
+            <button
+              className={cn(
+                'w-full rounded px-2 py-1 text-start text-sm transition duration-150',
+                selectedPage === 'test'
+                  ? 'bg-neutral-700'
+                  : 'hover:bg-neutral-700/50',
+              )}
+              onClick={() => setSelectedPage('test')}
+            >
+              test menu
+            </button>
+          </div>
+        </div>
 
-      <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-        <DialogPanel
-          transition
-          className="flex h-full max-h-[80%] w-full max-w-screen-xl select-none flex-col gap-2 rounded-xl bg-neutral-800 duration-200 ease-out data-[closed]:scale-95 data-[closed]:opacity-0"
-        >
-          <div className="flex h-full w-full flex-row">
-            <div className="w-[30%] rounded-xl p-4">
-              <DialogTitle className="text-xl font-semibold">
-                Settings
-              </DialogTitle>
-              <div className="mt-2 flex flex-col gap-1">
-                <button
-                  className={cn(
-                    'w-full rounded px-2 py-1 text-start text-sm transition duration-150',
-                    selectedPage === 'hotkeys'
-                      ? 'bg-neutral-700'
-                      : 'hover:bg-neutral-700/50',
-                  )}
-                  onClick={() => setSelectedPage('hotkeys')}
-                >
-                  Hotkeys
-                </button>
-                <button
-                  className={cn(
-                    'w-full rounded px-2 py-1 text-start text-sm transition duration-150',
-                    selectedPage === 'programInfo'
-                      ? 'bg-neutral-700'
-                      : 'hover:bg-neutral-700/50',
-                  )}
-                  onClick={() => setSelectedPage('programInfo')}
-                >
-                  Program Info
-                </button>
-                <button
-                  className={cn(
-                    'w-full rounded px-2 py-1 text-start text-sm transition duration-150',
-                    selectedPage === 'test'
-                      ? 'bg-neutral-700'
-                      : 'hover:bg-neutral-700/50',
-                  )}
-                  onClick={() => setSelectedPage('test')}
-                >
-                  test menu
-                </button>
+        <div className="h-full w-full p-6">
+          {/* Hotkeys */}
+          {selectedPage === 'hotkeys' && (
+            <div>
+              <h3 className="text-xl font-bold">Hotkeys</h3>
+              <div className="mt-2 text-sm text-neutral-500">
+                *Changing hotkeys are not yet supported.
+              </div>
+              <div className="mt-2">
+                <div className="rounded bg-neutral-800 px-3 py-1 text-gray-400">
+                  General
+                </div>
+                <div className="flex flex-col">
+                  <div className="mt-1 flex flex-row items-center gap-2">
+                    <span className="grow px-3">Open from file</span>
+                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                      Ctrl + O
+                    </div>
+                  </div>
+                  <div className="mt-1 flex flex-row items-center gap-2">
+                    <span className="grow px-3">Save to file</span>
+                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                      Ctrl + S
+                    </div>
+                  </div>
+                  <div className="mt-1 flex flex-row items-center gap-2">
+                    <span className="grow px-3">Open Settings</span>
+                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                      Ctrl + ,
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-1">
+                <div className="rounded bg-neutral-800 px-3 py-1 text-gray-400">
+                  Editor
+                </div>
+                <div className="flex flex-col">
+                  <div className="mt-1 flex flex-row items-center gap-2">
+                    <span className="grow px-3">Translate mode</span>
+                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                      T
+                    </div>
+                  </div>
+                  <div className="mt-1 flex flex-row items-center gap-2">
+                    <span className="grow px-3">Rotate mode</span>
+                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                      R
+                    </div>
+                  </div>
+                  <div className="mt-1 flex flex-row items-center gap-2">
+                    <span className="grow px-3">Scale mode</span>
+                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                      S
+                    </div>
+                  </div>
+                  <div className="mt-1 flex flex-row items-center gap-2">
+                    <span className="grow px-3">Group/Ungroup</span>
+                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                      G
+                    </div>
+                  </div>
+                  <div className="mt-1 flex flex-row items-center gap-2">
+                    <span className="grow px-3">Delete Entity</span>
+                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
+                      Delete
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
+          )}
 
-            <div className="h-full w-full rounded-xl bg-neutral-900/70 p-6">
-              {/* Hotkeys */}
-              {selectedPage === 'hotkeys' && (
-                <div>
-                  <h3 className="text-2xl font-bold">Hotkeys</h3>
-                  <div className="mt-2 text-sm text-neutral-500">
-                    *Changing hotkeys are not yet supported.
-                  </div>
-                  <div className="mt-2">
-                    <div className="rounded bg-neutral-800 px-3 py-1 text-gray-400">
-                      General
-                    </div>
-                    <div className="flex flex-col">
-                      <div className="mt-1 flex flex-row items-center gap-2">
-                        <span className="grow px-3">Open from file</span>
-                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                          Ctrl + O
-                        </div>
-                      </div>
-                      <div className="mt-1 flex flex-row items-center gap-2">
-                        <span className="grow px-3">Save to file</span>
-                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                          Ctrl + S
-                        </div>
-                      </div>
-                      <div className="mt-1 flex flex-row items-center gap-2">
-                        <span className="grow px-3">Open Settings</span>
-                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                          Ctrl + ,
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="mt-1">
-                    <div className="rounded bg-neutral-800 px-3 py-1 text-gray-400">
-                      Editor
-                    </div>
-                    <div className="flex flex-col">
-                      <div className="mt-1 flex flex-row items-center gap-2">
-                        <span className="grow px-3">Translate mode</span>
-                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                          T
-                        </div>
-                      </div>
-                      <div className="mt-1 flex flex-row items-center gap-2">
-                        <span className="grow px-3">Rotate mode</span>
-                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                          R
-                        </div>
-                      </div>
-                      <div className="mt-1 flex flex-row items-center gap-2">
-                        <span className="grow px-3">Scale mode</span>
-                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                          S
-                        </div>
-                      </div>
-                      <div className="mt-1 flex flex-row items-center gap-2">
-                        <span className="grow px-3">Group/Ungroup</span>
-                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                          G
-                        </div>
-                      </div>
-                      <div className="mt-1 flex flex-row items-center gap-2">
-                        <span className="grow px-3">Delete Entity</span>
-                        <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                          Delete
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              )}
+          {/* Program Info */}
+          {selectedPage === 'programInfo' && (
+            <div>
+              <h3 className="text-xl font-bold">Display Entity Platform</h3>
+              <div>Graphical editor for Minecraft display entities</div>
+              <div className="mt-4 flex flex-row items-center gap-2">
+                <span>v{__VERSION__}</span>
+                <span className="font-mono">{__COMMIT_HASH__}</span>
+                {__IS_DEV__ && <span>(Development Build)</span>}
+              </div>
 
-              {/* Program Info */}
-              {selectedPage === 'programInfo' && (
-                <div>
-                  <h3 className="text-2xl font-bold">
-                    Display Entity Platform
-                  </h3>
-                  <div>Graphical editor for Minecraft display entities</div>
-                  <div className="mt-4 flex flex-row items-center gap-2">
-                    <span>v{__VERSION__}</span>
-                    <span className="font-mono">{__COMMIT_HASH__}</span>
-                    {__IS_DEV__ && <span>(Development Build)</span>}
-                  </div>
-
-                  {/* Disclaimer */}
-                  <div className="mt-4 text-sm text-neutral-500">
-                    This website or tool is not an official Minecraft product.
-                    Minecraft is a trademark of Mojang AB. All rights related to
-                    Minecraft and its intellectual property are owned by Mojang
-                    AB.
-                  </div>
-                </div>
-              )}
-
-              {/* test page */}
-              {selectedPage === 'test' && (
-                <div>
-                  <div className="flex flex-row items-center gap-2">
-                    <input
-                      type="checkbox"
-                      id="settings_test_testoption"
-                      checked={settings.testOption}
-                      onChange={(evt) => {
-                        setSettings({ testOption: evt.target.checked })
-                      }}
-                    />
-                    <label htmlFor="settings_test_testoption">
-                      Test Option
-                    </label>
-                  </div>
-                  <div className="flex flex-row items-center gap-2">
-                    <label htmlFor="settings_test_minloglevel">
-                      Minimum Log Level
-                    </label>
-                    <select
-                      id="settings_test_minloglevel"
-                      className="flex-none rounded bg-neutral-800 px-2 py-1"
-                      value={settings.minLogLevel}
-                      onChange={(evt) => {
-                        setSettings({
-                          minLogLevel: evt.target.value as LogLevel,
-                        })
-                      }}
-                    >
-                      <option>error</option>
-                      <option>warn</option>
-                      <option>info</option>
-                      <option>debug</option>
-                    </select>
-                  </div>
-                </div>
-              )}
+              {/* Disclaimer */}
+              <div className="mt-4 text-sm text-neutral-500">
+                This website or tool is not an official Minecraft product.
+                Minecraft is a trademark of Mojang AB. All rights related to
+                Minecraft and its intellectual property are owned by Mojang AB.
+              </div>
             </div>
-          </div>
-        </DialogPanel>
+          )}
+
+          {/* test page */}
+          {selectedPage === 'test' && (
+            <div>
+              <div className="flex flex-row items-center gap-2">
+                <input
+                  type="checkbox"
+                  id="settings_test_testoption"
+                  checked={settings.testOption}
+                  onChange={(evt) => {
+                    setSettings({ testOption: evt.target.checked })
+                  }}
+                />
+                <label htmlFor="settings_test_testoption">Test Option</label>
+              </div>
+              <div className="flex flex-row items-center gap-2">
+                <label htmlFor="settings_test_minloglevel">
+                  Minimum Log Level
+                </label>
+                <select
+                  id="settings_test_minloglevel"
+                  className="flex-none rounded bg-neutral-800 px-2 py-1"
+                  value={settings.minLogLevel}
+                  onChange={(evt) => {
+                    setSettings({
+                      minLogLevel: evt.target.value as LogLevel,
+                    })
+                  }}
+                >
+                  <option>error</option>
+                  <option>warn</option>
+                  <option>info</option>
+                  <option>debug</option>
+                </select>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </Dialog>
   )
