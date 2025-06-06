503fb9e9690bb0836ff2e6d267f8be3675d606b4
comjun04 <comjun04@gmail.com>
2025-06-06 15:01:27 +0900

    refactor(settings): split settings pages into individual components

--- packages/app/src/components/dialog/SettingsDialog.tsx
+++ packages/app/src/components/dialog/SettingsDialog.tsx
@@ -7,6 +7,9 @@ import { LogLevel } from '@/types'
 import { cn } from '@/utils'
 
 import Dialog from './Dialog'
+import DebugOptionsPage from './settings/DebugOptionsPage'
+import HotkeysPage from './settings/HotkeysPage'
+import ProgramInfoPage from './settings/ProgramInfoPage'
 
 type SettingsPageType = 'hotkeys' | 'programInfo' | 'debug'
 
@@ -90,166 +93,13 @@ const SettingsDialog: FC = () => {
 
         <div className="h-full w-full py-4 xs:px-4">
           {/* Hotkeys */}
-          {selectedPage === 'hotkeys' && (
-            <div>
-              <h3 className="text-xl font-bold">Hotkeys</h3>
-              <div className="mt-2 text-sm text-neutral-500">
-                *Changing hotkeys are not yet supported.
-              </div>
-              <div className="mt-2">
-                <div className="rounded bg-neutral-800 px-3 py-1 text-gray-400">
-                  General
-                </div>
-                <div className="flex flex-col">
-                  <div className="mt-1 flex flex-row items-center gap-2">
-                    <span className="grow px-3">Open from file</span>
-                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                      Ctrl + O
-                    </div>
-                  </div>
-                  <div className="mt-1 flex flex-row items-center gap-2">
-                    <span className="grow px-3">Save to file</span>
-                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                      Ctrl + S
-                    </div>
-                  </div>
-                  <div className="mt-1 flex flex-row items-center gap-2">
-                    <span className="grow px-3">Open Settings</span>
-                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                      Ctrl + ,
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-1">
-                <div className="rounded bg-neutral-800 px-3 py-1 text-gray-400">
-                  Editor
-                </div>
-                <div className="flex flex-col">
-                  <div className="mt-1 flex flex-row items-center gap-2">
-                    <span className="grow px-3">Translate mode</span>
-                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                      T
-                    </div>
-                  </div>
-                  <div className="mt-1 flex flex-row items-center gap-2">
-                    <span className="grow px-3">Rotate mode</span>
-                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                      R
-                    </div>
-                  </div>
-                  <div className="mt-1 flex flex-row items-center gap-2">
-                    <span className="grow px-3">Scale mode</span>
-                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                      S
-                    </div>
-                  </div>
-                  <div className="mt-1 flex flex-row items-center gap-2">
-                    <span className="grow px-3">Group/Ungroup</span>
-                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                      G
-                    </div>
-                  </div>
-                  <div className="mt-1 flex flex-row items-center gap-2">
-                    <span className="grow px-3">Delete Entity</span>
-                    <div className="w-full max-w-[12rem] rounded bg-neutral-700/70 px-2 py-1">
-                      Delete
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
+          {selectedPage === 'hotkeys' && <HotkeysPage />}
 
           {/* Program Info */}
-          {selectedPage === 'programInfo' && (
-            <div>
-              <h3 className="text-xl font-bold">Display Entity Platform</h3>
-              <div>Graphical editor for Minecraft display entities</div>
-              <div className="mt-4 flex flex-row items-center gap-2">
-                <span>v{__VERSION__}</span>
-                <span className="font-mono">{__COMMIT_HASH__}</span>
-                {__IS_DEV__ && <span>(Development Build)</span>}
-              </div>
-
-              {/* Disclaimer */}
-              <div className="mt-4 text-sm text-neutral-500">
-                This website or tool is not an official Minecraft product.
-                Minecraft is a trademark of Mojang AB. All rights related to
-                Minecraft and its intellectual property are owned by Mojang AB.
-              </div>
-            </div>
-          )}
+          {selectedPage === 'programInfo' && <ProgramInfoPage />}
 
           {/* Debug */}
-          {selectedPage === 'debug' && (
-            <div>
-              <h3 className="text-xl font-bold">Debug Options</h3>
-              <div className="mt-4 flex flex-row items-center gap-2">
-                <input
-                  type="checkbox"
-                  id="settings_debug_testoption"
-                  checked={settings.debug.testOption}
-                  onChange={(evt) => {
-                    setSettings({
-                      debug: { testOption: evt.target.checked },
-                    })
-                  }}
-                />
-                <label htmlFor="settings_debug_testoption">Test Option</label>
-              </div>
-              <div className="mt-4 flex flex-row items-center gap-2">
-                <label htmlFor="settings_debug_minloglevel">
-                  Minimum Log Level
-                </label>
-                <select
-                  id="settings_debug_minloglevel"
-                  className="flex-none rounded bg-neutral-900 px-2 py-1"
-                  value={settings.debug.minLogLevel}
-                  onChange={(evt) => {
-                    setSettings({
-                      debug: { minLogLevel: evt.target.value as LogLevel },
-                    })
-                  }}
-                >
-                  <option>error</option>
-                  <option>warn</option>
-                  <option>info</option>
-                  <option>debug</option>
-                </select>
-              </div>
-              <div className="mt-4 flex flex-row items-center gap-2">
-                <input
-                  type="checkbox"
-                  id="settings_debug_perfMonitorEnabled"
-                  checked={settings.debug.perfMonitorEnabled}
-                  onChange={(evt) => {
-                    setSettings({
-                      debug: { perfMonitorEnabled: evt.target.checked },
-                    })
-                  }}
-                />
-                <label htmlFor="settings_debug_perfMonitorEnabled">
-                  Enable Performance Monitor
-                </label>
-              </div>
-              <div className="mt-4 flex flex-row items-center gap-2">
-                <input
-                  type="checkbox"
-                  id="settings_debug_alertUncaughtError"
-                  checked={settings.debug.alertUncaughtError}
-                  onChange={(evt) => {
-                    setSettings({
-                      debug: { alertUncaughtError: evt.target.checked },
-                    })
-                  }}
-                />
-                <label htmlFor="settings_debug_alertUncaughtError">
-                  Notify uncaught errors with window.alert()
-                </label>
-              </div>
-            </div>
-          )}
+          {selectedPage === 'debug' && <DebugOptionsPage />}
         </div>
       </div>
     </Dialog>
