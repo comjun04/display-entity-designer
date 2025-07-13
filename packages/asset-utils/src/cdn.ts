9cc4650c746cb00af7ae70029c6a607ff1d73964
comjun04 <comjun04@gmail.com>
2025-07-13 20:32:29 +0900

    feat(asset-utils): skip already downloaded jar files

--- packages/asset-utils/src/cdn.ts
+++ packages/asset-utils/src/cdn.ts
@@ -1,4 +1,6 @@
+import { createHash } from 'crypto'
 import { createWriteStream } from 'fs'
+import { readFile } from 'fs/promises'
 import { Readable } from 'stream'
 import { finished } from 'stream/promises'
 import { join as pathJoin } from 'path'
@@ -20,19 +22,37 @@ async function fetchVersionData(versionId: string) {
   return clientJson
 }
 
+async function calculateFileHash(filePath: string) {
+  const buf = await readFile(filePath)
+  const hash = createHash('sha1').update(buf).digest('hex')
+  return hash
+}
+
 export async function downloadAssets(versionId: string, baseDirPath: string) {
   // First fetch client.json to check the data of that version
   const versionData = await fetchVersionData(versionId)
-  
-  // download client.jar and server.jar
-  console.log('Downloading client.jar from Mojang CDN...')
-  const clientJarWriteStream = createWriteStream(pathJoin(baseDirPath, 'client.jar'))
-  const clientJarFetchResponse = await fetch(versionData.downloads.client.url)
-  await finished(Readable.fromWeb(clientJarFetchResponse.body).pipe(clientJarWriteStream))
 
-  console.log('Downloading server.jar from Mojang CDN...')
-  const serverJarWriteStream = createWriteStream(pathJoin(baseDirPath, 'server.jar'))
-  const serverJarFetchResponse = await fetch(versionData.downloads.server.url)
-  await finished(Readable.fromWeb(serverJarFetchResponse.body).pipe(serverJarWriteStream))
+  // Check hash of existing client.jar file
+  // download if file not exists or hash mismatch
+  console.log('Checking for existing client.jar file')
+  const clientJarFilePath = pathJoin(baseDirPath, 'client.jar')
+  const existingClientJarHash = await calculateFileHash(clientJarFilePath).catch(_ => null)
+  if (existingClientJarHash !== versionData.downloads.client.sha1) {
+    console.log('Downloading client.jar from Mojang CDN...')
+    const clientJarWriteStream = createWriteStream(clientJarFilePath)
+    const clientJarFetchResponse = await fetch(versionData.downloads.client.url)
+    await finished(Readable.fromWeb(clientJarFetchResponse.body).pipe(clientJarWriteStream))
+  }
+  
+  // same thing goes for server.jar
+  console.log('Checking for existing server.jar file')
+  const serverJarFilePath = pathJoin(baseDirPath, 'server.jar')
+  const existingServerJarHash = await calculateFileHash(serverJarFilePath).catch(_ => null)
+  if (existingServerJarHash !== versionData.downloads.server.sha1) {
+    console.log('Downloading server.jar from Mojang CDN...')
+    const serverJarWriteStream = createWriteStream(serverJarFilePath)
+    const serverJarFetchResponse = await fetch(versionData.downloads.server.url)
+    await finished(Readable.fromWeb(serverJarFetchResponse.body).pipe(serverJarWriteStream))
+  }
 }
 
