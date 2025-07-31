81540dbd22401d981ce6337e9606cb1f7b9aedc1
comjun04 <comjun04@gmail.com>
2025-07-31 18:31:01 +0900

    fix(fetcher): handle both slash-prefixed and non-prefixed urls

--- packages/app/src/fetcher.ts
+++ packages/app/src/fetcher.ts
@@ -1,6 +1,8 @@
 import { CDNVersionAssetsUrl } from './constants'
 
 export default async function fetcher(url: string) {
+  const slashPrefixedUrl = url.length > 0 && url[0] !== '/' ? `/${url}` : url
+
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return fetch(`${CDNVersionAssetsUrl}/${url}`).then((r) => r.json())
+  return fetch(`${CDNVersionAssetsUrl}${slashPrefixedUrl}`).then((r) => r.json())
 }
