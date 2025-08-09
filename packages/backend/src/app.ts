f535b056cc65f671398c4d538922db994e680d33
comjun04 <comjun04@gmail.com>
2025-08-09 12:31:30 +0900

    feat(backend): add player skin query api

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,5 +1,40 @@
 import { Hono } from 'hono'
 
+import { PlayerSkinQueryResult, ProfileLookupResult, TextureValue } from './types'
+
 const app = new Hono()
+
+app.get('/v1/skin/:usernameOrUuid', async (c) => {
+  const { usernameOrUuid } = c.req.param()
+
+  let type: 'username' | 'uuid'
+  if (/^[a-z0-9_]{3,16}$/gi.test(usernameOrUuid)) {
+    type = 'username'
+  } else if (/^[0-9a-f]{8}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{12}$/gi.test(usernameOrUuid)) {
+    type = 'uuid'
+  } else {
+    c.status(400)
+    return c.json({ error: 'Invalid username or uuid' })
+  }
+
+  let uuid: string
+  if (type === 'username') {
+    const profileLookupResult = await fetch(`https://api.minecraftservices.com/minecraft/profile/lookup/name/${usernameOrUuid}`).then(res => res.json()) as ProfileLookupResult
+    uuid = profileLookupResult.id
+  } else {
+    uuid = usernameOrUuid
+  }
+
+  uuid = uuid.replaceAll('-', '')
+
+  const skinQueryResult = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`).then(res => res.json()) as PlayerSkinQueryResult
+  const textureValue = JSON.parse(Buffer.from(skinQueryResult.properties[0].value, 'base64').toString()) as TextureValue
+
+  return c.json({
+    id: skinQueryResult.id,
+    name: skinQueryResult.name,
+    skinUrl: textureValue.textures.SKIN?.url ?? null
+  })
+})
 
 export default app
