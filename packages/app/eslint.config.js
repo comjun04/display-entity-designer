4b3fb968f9810b1154e1fafd6600112eeedfd8bf
comjun04 <comjun04@gmail.com>
2025-09-27 13:29:34 +0000

    refactor(app/eslint): do not flag error for unused vars starting with `_`

--- packages/app/eslint.config.js
+++ packages/app/eslint.config.js
@@ -1,8 +1,8 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -36,6 +36,18 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       'react/no-unknown-property': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
     },
   },
 )
