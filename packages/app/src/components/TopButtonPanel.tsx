279101592bd510a2d00fb71f8a86ea9b5f225b0a
comjun04 <comjun04@gmail.com>
2025-10-02 10:48:11 +0900

    feat(app): show adding display entity button to dropdown on mobile

--- packages/app/src/components/TopButtonPanel.tsx
+++ packages/app/src/components/TopButtonPanel.tsx
@@ -3,7 +3,15 @@ import type { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { IoMdTrash } from 'react-icons/io'
 import { IoCubeOutline } from 'react-icons/io5'
-import { LuCopyPlus, LuGroup, LuSmile, LuType, LuUngroup } from 'react-icons/lu'
+import {
+  LuChevronDown,
+  LuCopyPlus,
+  LuGroup,
+  LuPlus,
+  LuSmile,
+  LuType,
+  LuUngroup,
+} from 'react-icons/lu'
 import { TbDiamondFilled } from 'react-icons/tb'
 import { useShallow } from 'zustand/shallow'
 
@@ -12,6 +20,12 @@ import { useDialogStore } from '@/stores/dialogStore'
 import { useDisplayEntityStore } from '@/stores/displayEntityStore'
 
 import FloatingButton from './FloatingButton'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/DropdownMenu'
 
 const TopButtonPanel: FC = () => {
   const { t } = useTranslation()
@@ -35,58 +49,107 @@ const TopButtonPanel: FC = () => {
   return (
     <div className="absolute left-1/2 top-4 z-[5] -translate-x-1/2">
       <div className="flex flex-row rounded-lg bg-black">
-        <Tooltip
-          content={t(($) => $.editor.topBar.blockDisplay)}
-          placement="bottom"
-          size="sm"
-          offset={0}
-          delay={300}
-          closeDelay={0}
-        >
-          <FloatingButton
-            onClick={() => {
-              setOpenedDialog('blockDisplaySelect')
-            }}
+        {/* Desktop - show all 'Add Display Entity' buttons */}
+        <div className="flex hidden flex-row sm:block">
+          <Tooltip
+            content={t(($) => $.editor.topBar.blockDisplay)}
+            placement="bottom"
+            size="sm"
+            offset={0}
+            delay={300}
+            closeDelay={0}
           >
-            <IoCubeOutline size={24} />
-          </FloatingButton>
-        </Tooltip>
+            <FloatingButton
+              onClick={() => {
+                setOpenedDialog('blockDisplaySelect')
+              }}
+            >
+              <IoCubeOutline size={24} />
+            </FloatingButton>
+          </Tooltip>
 
-        <Tooltip
-          content={t(($) => $.editor.topBar.itemDisplay)}
-          placement="bottom"
-          size="sm"
-          offset={0}
-          delay={300}
-          closeDelay={0}
-        >
-          <FloatingButton
-            onClick={() => {
-              setOpenedDialog('itemDisplaySelect')
-            }}
+          <Tooltip
+            content={t(($) => $.editor.topBar.itemDisplay)}
+            placement="bottom"
+            size="sm"
+            offset={0}
+            delay={300}
+            closeDelay={0}
           >
-            <TbDiamondFilled size={24} />
-          </FloatingButton>
-        </Tooltip>
+            <FloatingButton
+              onClick={() => {
+                setOpenedDialog('itemDisplaySelect')
+              }}
+            >
+              <TbDiamondFilled size={24} />
+            </FloatingButton>
+          </Tooltip>
 
-        <Tooltip
-          content={t(($) => $.editor.topBar.textDisplay)}
-          placement="bottom"
-          size="sm"
-          offset={0}
-          delay={300}
-          closeDelay={0}
-        >
-          <FloatingButton
-            onClick={() => {
-              useDisplayEntityStore
-                .getState()
-                .createNew([{ kind: 'text', text: 'Enter Text' }])
-            }}
+          <Tooltip
+            content={t(($) => $.editor.topBar.textDisplay)}
+            placement="bottom"
+            size="sm"
+            offset={0}
+            delay={300}
+            closeDelay={0}
           >
-            <LuType size={24} />
-          </FloatingButton>
-        </Tooltip>
+            <FloatingButton
+              onClick={() => {
+                useDisplayEntityStore
+                  .getState()
+                  .createNew([{ kind: 'text', text: 'Enter Text' }])
+              }}
+            >
+              <LuType size={24} />
+            </FloatingButton>
+          </Tooltip>
+        </div>
+        {/* Mobile - show dropdown menu for 'Add Display Entity' action */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <FloatingButton className="flex flex-row items-center gap-1 sm:hidden">
+              <LuPlus size={24} />
+              <LuChevronDown size={16} />
+            </FloatingButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            side="bottom"
+            align="start"
+            className="origin-top-left"
+          >
+            <DropdownMenuItem
+              className="flex flex-row items-center gap-2"
+              onClick={() => {
+                setOpenedDialog('blockDisplaySelect')
+              }}
+            >
+              <IoCubeOutline />
+              {t(($) => $.editor.topBar.blockDisplay)}
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="flex flex-row items-center gap-2"
+              onClick={() => {
+                setOpenedDialog('itemDisplaySelect')
+              }}
+            >
+              <TbDiamondFilled />
+
+              {t(($) => $.editor.topBar.itemDisplay)}
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="flex flex-row items-center gap-2"
+              onClick={() => {
+                useDisplayEntityStore
+                  .getState()
+                  .createNew([{ kind: 'text', text: 'Enter Text' }])
+              }}
+            >
+              <LuType />
+
+              {t(($) => $.editor.topBar.textDisplay)}
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
 
         <div className="my-2 border-l border-gray-700" />
 
