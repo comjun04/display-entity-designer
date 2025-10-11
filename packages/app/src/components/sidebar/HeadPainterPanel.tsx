11bd35b67744e59fcd12b80384b3520a1d0bff59
comjun04 <comjun04@gmail.com>
2025-10-11 15:56:14 +0900

    feat(app/sidebar): add bake textures section to HeadPainterPanel

--- packages/app/src/components/sidebar/HeadPainterPanel.tsx
+++ packages/app/src/components/sidebar/HeadPainterPanel.tsx
@@ -1,7 +1,9 @@
 import { type FC } from 'react'
 import { useShallow } from 'zustand/shallow'
 
+import { useDisplayEntityStore } from '@/stores/displayEntityStore'
 import { useEditorStore } from '@/stores/editorStore'
+import { isItemDisplayPlayerHead } from '@/types'
 import { cn } from '@/utils'
 
 import { SidePanel, SidePanelContent, SidePanelTitle } from '../SidePanel'
@@ -14,12 +16,28 @@ const HeadPainterPanel: FC = () => {
       layer: state.headPainter.layer,
     })),
   )
+  const unbakedHeadsCount = useDisplayEntityStore((state) => {
+    let count = 0
+    for (const entity of state.entities.values()) {
+      if (
+        isItemDisplayPlayerHead(entity) &&
+        entity.playerHeadProperties.texture?.baked === false
+      ) {
+        count++
+      }
+    }
+
+    return count
+  })
 
   return (
     <SidePanel>
       <SidePanelTitle>Head Painter</SidePanelTitle>
       <SidePanelContent>
         <div className="flex flex-col gap-2">
+          <div className="rounded bg-neutral-700 p-1 px-2 text-xs font-bold text-neutral-400">
+            Paint
+          </div>
           <div className="flex flex-row items-center gap-2">
             <label className="flex-1 text-end">Brush Color</label>
             <ColorPickerInput
@@ -58,6 +76,33 @@ const HeadPainterPanel: FC = () => {
                 Second
               </button>
             </div>
+          </div>
+
+          <div className="rounded bg-neutral-700 p-1 px-2 text-xs font-bold text-neutral-400">
+            Bake textures
+          </div>
+          <div className="flex flex-col gap-2">
+            <div>
+              Player heads with painted textures applied need to have their
+              textures uploaded as Minecraft skin to use in-game.
+            </div>
+            {unbakedHeadsCount > 0 ? (
+              <>
+                <span>
+                  There {unbakedHeadsCount === 1 ? 'is' : 'are'}{' '}
+                  {unbakedHeadsCount}{' '}
+                  {unbakedHeadsCount === 1 ? 'head' : 'heads'} with unbaked
+                  texture.
+                </span>
+                <button className="rounded bg-stone-700 p-1">
+                  Bake textures
+                </button>
+              </>
+            ) : (
+              <span className="text-gray-500">
+                All textures used on player heads are baked.
+              </span>
+            )}
           </div>
         </div>
       </SidePanelContent>
