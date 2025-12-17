006324caac3ddab1269ed9d62847c1edf6d7e7c1
comjun04 <comjun04@gmail.com>
2025-12-17 14:54:45 +0000

    feat(app): add blockstates preloading and log loading duration

--- packages/app/src/services/resources/preload.ts
+++ packages/app/src/services/resources/preload.ts
@@ -18,23 +18,42 @@ export async function preloadResources(entities: DisplayEntity[]) {
   })[] = []
 
   logger.log('preLoadResources(): start preloading resources')
+  const preLoadingStartTime = performance.now()
 
-  for (const entity of entities) {
-    if (entity.kind === 'block') {
-      const blockstatesData = await loadBlockstates(entity.type)
+  const blockstatesBatchLoadStartTime = preLoadingStartTime
+  const blockstatesBatchLoadResults = await Promise.allSettled(
+    entities.map(async (entity) => {
+      if (entity.kind === 'block') {
+        const blockstatesData = await loadBlockstates(entity.type)
 
-      const aa = getMatchingBlockstateModel(blockstatesData, entity.blockstates)
-      applyModelInfoList.push(...aa)
-    } else if (entity.kind === 'item') {
-      applyModelInfoList.push({
-        model: `item/${entity.type}`,
-        playerHeadTextureData: isItemDisplayPlayerHead(entity)
-          ? (entity.playerHeadProperties.texture ?? undefined)
-          : undefined,
-      })
-    }
-  }
+        const matchingModelInfoArr = getMatchingBlockstateModel(
+          blockstatesData,
+          entity.blockstates,
+        )
+        applyModelInfoList.push(...matchingModelInfoArr)
+      } else if (entity.kind === 'item') {
+        applyModelInfoList.push({
+          model: `item/${entity.type}`,
+          playerHeadTextureData: isItemDisplayPlayerHead(entity)
+            ? (entity.playerHeadProperties.texture ?? undefined)
+            : undefined,
+        })
+      }
+    }),
+  )
 
+  const blockstatesBatchLoadTimeDiff = Math.round(
+    performance.now() - blockstatesBatchLoadStartTime,
+  )
+  const blockstatesBatchLoadSuccessCount = blockstatesBatchLoadResults.filter(
+    (d) => d.status === 'fulfilled',
+  ).length
+  logger.log(
+    `preLoadResources(): batch loaded entity blockstates data, took ${blockstatesBatchLoadTimeDiff}ms.`,
+    `Total ${blockstatesBatchLoadResults.length}, success ${blockstatesBatchLoadSuccessCount}, failed ${blockstatesBatchLoadResults.length - blockstatesBatchLoadSuccessCount}`,
+  )
+
+  const batchLoadStartTime = performance.now()
   const batchLoadResults = await Promise.allSettled(
     applyModelInfoList.map(async (applyModelInfo) => {
       const modelData = await loadModel(applyModelInfo.model)
@@ -49,12 +68,18 @@ export async function preloadResources(entities: DisplayEntity[]) {
       })
     }),
   )
+
+  const batchLoadTimeDiff = Math.round(performance.now() - batchLoadStartTime)
   const batchLoadSuccessCount = batchLoadResults.filter(
     (d) => d.status === 'fulfilled',
   ).length
-  logger.debug(
-    `preLoadResources(): total ${batchLoadResults.length}, success ${batchLoadSuccessCount}, failed ${batchLoadResults.length - batchLoadSuccessCount}`,
+  logger.log(
+    `preLoadResources(): batch loaded entity model data and material assets, took ${batchLoadTimeDiff}ms.`,
+    `Total ${batchLoadResults.length}, success ${batchLoadSuccessCount}, failed ${batchLoadResults.length - batchLoadSuccessCount}`,
   )
 
-  logger.log('preLoadResources(): finish preloading resources')
+  const preLoadingTimeDiff = Math.round(performance.now() - preLoadingStartTime)
+  logger.log(
+    `preLoadResources(): finish preloading resources, took ${preLoadingTimeDiff}ms`,
+  )
 }
